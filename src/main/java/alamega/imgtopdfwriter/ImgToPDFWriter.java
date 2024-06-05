a439a6bae6c52e2bb3e0eb05a35b60a515a95b1e
Alamega <vladshuman@gmail.com>
2024-06-05 17:17:31 +0300

    интерфейс

--- src/main/java/alamega/imgtopdfwriter/ImgToPDFWriter.java
+++ src/main/java/alamega/imgtopdfwriter/ImgToPDFWriter.java
@@ -3,13 +3,21 @@ package alamega.imgtopdfwriter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class ImgToPDFWriter extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        //Настройка Stage
+        stage.setMinWidth(480);
+        stage.setMinHeight(200);
+        stage.getIcons().add(new Image(Objects.requireNonNull(ImgToPDFWriter.class.getResourceAsStream("/logo.png"))));
+
+        //Создание и настройка Scene
         FXMLLoader fxmlLoader = new FXMLLoader(ImgToPDFWriter.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Запиши чтобы не забыть! Что?");
--- src/main/java/alamega/imgtopdfwriter/MainController.java
+++ src/main/java/alamega/imgtopdfwriter/MainController.java
@@ -1,14 +1,47 @@
 package alamega.imgtopdfwriter;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button buttonFileChoice;
+
+    @FXML
+    private Button buttonPathForOutput;
+
+    @FXML
+    private Button buttonWrite;
+
+    @FXML
+    private TextField inputCountWrite;
+
+    @FXML
+    private Label labelFileCount;
+
+    @FXML
+    private Label labelPathForOutput;
+
+    @FXML
+    void initialize() {
+        assert buttonFileChoice != null : "fx:id=\"buttonFileChoice\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert buttonPathForOutput != null : "fx:id=\"buttonPathForOutput\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert buttonWrite != null : "fx:id=\"buttonWrite\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert inputCountWrite != null : "fx:id=\"inputCountWrite\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert labelFileCount != null : "fx:id=\"labelFileCount\" was not injected: check your FXML file 'main-view.fxml'.";
+        assert labelPathForOutput != null : "fx:id=\"labelPathForOutput\" was not injected: check your FXML file 'main-view.fxml'.";
+
     }
+
 }
